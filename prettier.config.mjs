b3c871b2dea59dcf053ae81a8a259e4a1a736937
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-02-18 19:51:11 +0100

    Add development tools

---
+++ prettier.config.mjs
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  singleQuote: true,
+  overrides: [
+    {
+      files: '*.jsx',
+      options: {
+        printWidth: 100,
+      },
+    },
+  ],
+};
