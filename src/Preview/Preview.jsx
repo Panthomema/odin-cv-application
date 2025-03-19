2bdb451ddcb8f0b3a1ce41b76cf22afd8616140d
Panthomema <71283471+Panthomema@users.noreply.github.com>
2025-03-20 00:05:33 +0100

    Refactor Preview.jsx

--- src/Preview/Preview.jsx
+++ src/Preview/Preview.jsx
@@ -5,10 +5,9 @@ import styles from './Preview.module.css';
 const A4_WIDTH = 794;
 const A4_HEIGHT = 1123;
 
-export default function Preview({ resumeData }) {
+export default function Preview({ resumeData: { personal, experience } }) {
   const [scale, setScale] = useState(1);
   const containerRef = useRef(null);
-  const { personal, experience } = resumeData;
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -38,84 +37,94 @@ export default function Preview({ resumeData }) {
 
     // Clean up
     return () => {
-      resizeObserver.unobserve(containerDiv);
       resizeObserver.disconnect();
     };
-  }, []);
+  }, [scale]);
 
   return (
     <div className={styles.previewContainer} ref={containerRef}>
       <div className={styles.preview} style={{ transform: `scale(${scale})` }}>
-        <div className={styles.personal}>
-          <h1>{personal.fullName}</h1>
-          <ul className={styles.contact}>
-            {personal.email && (
-              <li>
-                <span className="material-symbols-outlined">mail</span>
-                {personal.email}
-              </li>
-            )}
-            {personal.phoneNumber && (
-              <li>
-                <span className="material-symbols-outlined">call</span>
-                {personal.phoneNumber}
-              </li>
-            )}
-            {personal.location && (
-              <li>
-                <span className="material-symbols-outlined">location_on</span>
-                {personal.location}
-              </li>
-            )}
-          </ul>
-        </div>
+        <PersonalSection personal={personal} />
         <div className={styles.content}>
-          {experience.some(({ visible }) => visible) && (
-            <div className={styles.section}>
-              <h2 className={styles.sectionTitle}>Professional Experience</h2>
-              {experience
-                .filter(({ visible }) => visible)
-                .map((item) => (
-                  <div key={item.id} className={styles.sectionItem}>
-                    <div className={styles.itemHeader}>
-                      <div>
-                        <p>
-                          <strong>{item.companyName}</strong>
-                        </p>
-                        {item.position && (
-                          <p>
-                            <em>{item.position}</em>
-                          </p>
-                        )}
-                      </div>
-                      <div>
-                        {item.startDate && (
-                          <p className={styles.itemDate}>
-                            {format(new Date(item.startDate), 'MMMM yyyy')} -{' '}
-                            {item.endDate !== ''
-                              ? format(new Date(item.endDate), 'MMMM yyyy')
-                              : 'Present'}
-                          </p>
-                        )}
-                        {item.location && <p>{item.location}</p>}
-                      </div>
-                    </div>
-                    {item.description && (
-                      <p>
-                        {item.description.split('\n').map((line, index) => (
-                          <span key={`${item.id}-${index}`}>
-                            {line}
-                            <br />
-                          </span>
-                        ))}
-                      </p>
-                    )}
-                  </div>
-                ))}
-            </div>
-          )}
+          <ExperenceSection experience={experience} />
         </div>
       </div>
     </div>
+  );
+}
+
+function PersonalSection({ personal }) {
+  return (
+    <div className={styles.personal}>
+      <h1>{personal.fullName}</h1>
+      <ul className={styles.contact}>
+        {personal.email && (
+          <li>
+            <span className="material-symbols-outlined">mail</span>
+            {personal.email}
+          </li>
+        )}
+        {personal.phoneNumber && (
+          <li>
+            <span className="material-symbols-outlined">call</span>
+            {personal.phoneNumber}
+          </li>
+        )}
+        {personal.location && (
+          <li>
+            <span className="material-symbols-outlined">location_on</span>
+            {personal.location}
+          </li>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+function ExperenceSection({ experience }) {
+  const visibleExperience = experience.filter(({ visible }) => visible);
+  return (
+    visibleExperience.length > 0 && (
+      <div className={styles.section}>
+        <h2 className={styles.sectionTitle}>Professional Experience</h2>
+        {visibleExperience.map((item) => (
+          <div key={item.id} className={styles.sectionItem}>
+            <div className={styles.itemHeader}>
+              <div>
+                <p>
+                  <strong>{item.companyName}</strong>
+                </p>
+                {item.position && (
+                  <p>
+                    <em>{item.position}</em>
+                  </p>
+                )}
+              </div>
+              <div>
+                {item.startDate && (
+                  <p className={styles.itemDate}>
+                    {format(new Date(item.startDate), 'MMMM yyyy')} -{' '}
+                    {item.endDate !== ''
+                      ? format(new Date(item.endDate), 'MMMM yyyy')
+                      : 'Present'}
+                  </p>
+                )}
+                {item.location && <p>{item.location}</p>}
+              </div>
+            </div>
+            {item.description && (
+              <p>
+                {item.description.split('\n').map((line, index) => (
+                  <span key={`${item.id}-${index}`}>
+                    {line}
+                    <br />
+                  </span>
+                ))}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    )
   );
 }
